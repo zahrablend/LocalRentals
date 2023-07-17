5662365297fa0b676b353dbafaf554e7f1f0ad1f
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-17 09:42:44 +0300

    Document possible responce type for GetRentals method in RentalsApiontroller. Add validation with return types to GetRentals method.

--- LocalRentalsAPI/Controllers/RentalsAPIController.cs
+++ LocalRentalsAPI/Controllers/RentalsAPIController.cs
@@ -2,6 +2,7 @@
 using LocalRentalsApi.Models;
 using LocalRentalsApi.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 namespace LocalRentalsApi
     .Controllers
@@ -17,18 +18,39 @@ namespace LocalRentalsApi
     {
         // Specify GET endpoint for return statement
         [HttpGet]
-        public IEnumerable<RentalDto> GetRentals()
+        // Action result => implementation of Interface IAction instead of defining return type
+        public ActionResult<IEnumerable<RentalDto>> GetRentals()
         {
             // Returns all rentals on the list
-            return RentalData.rentalsList;
+            return Ok(RentalData.rentalsList);
         }
 
         [HttpGet("id:int")]
+        // Document responce code
+        [ProducesResponseType(200)] //OK
+        [ProducesResponseType(404)] // Not found
+        [ProducesResponseType(400)] // Bad request
         // [HttpGet("id")] => without type specified
-        public RentalDto GetRentals(int id)
+        public ActionResult<RentalDto?> GetRentals(int id)
         {
+            /*
+             FirstOrDefault is called on Enumerable whereas Find is called as a method on the source list.
+             Find method is iterating over an array of items (since a list is a wrapper on an array).
+             FirstOrDefault, on the Enumerable class, uses foreach to iterate the items (an iterator to the list and move next).
+             */
             // returning first record only
-            return RentalData.rentalsList.FirstOrDefault(v => v.Id == id);
+            var rental = RentalData.rentalsList.FirstOrDefault(rental => rental.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rental);
         }
     }
 }
